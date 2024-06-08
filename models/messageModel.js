7c255f06140336db26dd1fda9bbf4277c3ebb8f7
waliulohan <waliulislam1742@gmail.com>
2024-06-08 19:00:11 +0600

    Completed server

---
+++ models/messageModel.js
@@ -0,0 +1,35 @@
+const { Schema, model, Types } = require("mongoose");
+
+
+const messageModel = new Schema({
+    chat:{
+        type: Types.ObjectId,
+        ref: "Chat",
+        required: true,
+    },
+    sender:{
+        type: Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    content:{
+        type: String,
+    },
+    attachments: [
+        {
+          public_id: {
+            type: String,
+            required: true,
+          },
+          url: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
+},{
+    timestamps: true,
+})
+
+const Message = model("Message" , messageModel)
+module.exports = Message;
