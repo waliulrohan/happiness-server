d91723b390bfc9eae92c3904919dfd97faff8b69
waliulohan <waliulislam1742@gmail.com>
2024-06-10 17:08:01 +0600

    Added ckient URL

--- constants/config.js
+++ constants/config.js
@@ -1,7 +1,8 @@
 const corsOptions = {
     origin: [
       "http://localhost:5173",
-      "http://localhost:4173",
+      "https://happiness-v1.web.app",
+      process.env.CLIENT_URL,
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
