2c6fb3da4d0a2fe24fa9cc33a73bedd741404644
waliulohan <waliulislam1742@gmail.com>
2024-06-10 18:54:47 +0600

    done

--- utils/features.js
+++ utils/features.js
@@ -21,13 +21,14 @@ const cookieOptions = {
   maxAge: 15 * 24 * 60 * 60 * 1000,
   sameSite: "none",
   secure: true,
+  domain: 'happiness-v1.web.app',
 };
 
 
 const sendToken = (res, user, code, message) => {
   const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-  res.setHeader('Set-Cookie', `happiness-cookie=${token}; Max-Age=${15 * 24 * 60 * 60}; Secure; SameSite=None`);
-  return res.status(code).json({
+
+  return res.status(code).cookie("happiness-cookie", token, cookieOptions).json({
     success: true,
     user,
     message,
