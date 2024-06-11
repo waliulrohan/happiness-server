804ea618956c9e9324bd1c74efa0a185c8f2e379
waliulohan <waliulislam1742@gmail.com>
2024-06-11 18:15:29 +0600

    done

--- utils/features.js
+++ utils/features.js
@@ -22,7 +22,7 @@ const cookieOptions = {
   secure: true,
   sameSite: 'none',
   path: '/',
-  domain: "happiness-v1.web.app",
+  domain: ".web.app",
 };
 
 const sendToken = (res, user, code, message) => {
